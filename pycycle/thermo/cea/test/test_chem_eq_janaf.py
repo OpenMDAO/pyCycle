e63a07637628c69ff2370f03e9f86e56c1cfc88c
Eric Hendricks <eric.hendricks@nasa.gov>
2021-01-15 15:19:09 -0500

    cleaned up ChemEQ and tests

--- pycycle/thermo/cea/test/test_chem_eq_janaf.py
+++ pycycle/thermo/cea/test/test_chem_eq_janaf.py
@@ -35,34 +35,6 @@ class ChemEqTestCase(unittest.TestCase):
         print(check_val)
         assert_near_equal(p['n'], check_val, tol)
 
-    # def test_set_total_hp(self):
-    #     p = self.p
-    #     p.model.add_subsystem('ceq', ChemEq(thermo=self.thermo, mode="h"), promotes=["*"])
-    #     p.model.set_input_defaults('h', -24.3, units='cal/g')
-    #     p.setup(check=False)
-    #     p.run_model()
-    #     check_val = np.array([3.23319258e-04, 1.00000000e-10, 1.10131241e-05, 1.00000000e-10,
-    #                         1.00000000e-10, 1.00000000e-10, 1.00000000e-10, 2.69578866e-02,
-    #                         1.00000000e-10, 7.23199382e-03])
-
-    #     tol = 6e-4
-
-    #     assert_near_equal(p['n'], check_val, tol)
-
-    # def test_set_total_sp(self):
-    #     p = self.p
-    #     p.model.add_subsystem('ceq', ChemEq(thermo=self.thermo, mode="S"), promotes=["*"])
-    #     p.model.set_input_defaults('S', 1.58645, units='cal/(g*degK)')
-    #     p.setup(check=False)
-    #     p.run_model()
-
-    #     check_val = np.array([3.23319258e-04, 1.00000000e-10, 1.10131241e-05, 1.00000000e-10,
-    #                         1.00000000e-10, 1.00000000e-10, 1.00000000e-10, 2.69578866e-02,
-    #                         1.00000000e-10, 7.23199382e-03])
-
-    #     tol = 6e-4
-    #     assert_near_equal(p['n'], check_val, tol)
-
 
 if __name__ == "__main__":
 
